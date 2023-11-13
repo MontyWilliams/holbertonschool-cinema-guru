1a13173b5135d60fdeb91bca530be9db62ce42f2
Montywilliams <3650@holbertonschool.com>
2023-11-13 16:04:50 -0600

    add input props for components and create input box

--- src/components/general/Input.js
+++ src/components/general/Input.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './general.css'
+
 
 export default function Input({ label, type, className, value, setValue, icon, inputAttributes }) {
   return (
-    <div>
-      Input
-    </div>
+    <div className={`input-container ${className}`}>
+  <label>
+    {icon && (
+      <FontAwesomeIcon icon={icon} className="input-icon" />
+    )}
+    {label}:
+    <input
+      type={type}
+      value={value}
+     
+      {...inputAttributes}
+    />
+  </label>
+</div>
   )
 }
+
+
+
