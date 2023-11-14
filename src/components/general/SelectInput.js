e605c52f5e4bbcc79788afdb24c618168534503c
Montywilliams <3650@holbertonschool.com>
2023-11-14 13:39:08 -0600

    add div and label

--- src/components/general/SelectInput.js
+++ src/components/general/SelectInput.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function SelectInput({label, options, className, value, setValue}) {
   return (
-    <div>
-      SelectInput
-    </div>
+    <div className={`select-container ${className}`}>
+    <label>{label}
+      <select value={value} onChange={(e) => setValue(e.target.value)}>
+        {options.map((option, index) => {
+          return <option key={index} value={option.value}>{option.label}</option>
+        })}
+      </select>
+    </label>
+  </div>
   )
 }
 
