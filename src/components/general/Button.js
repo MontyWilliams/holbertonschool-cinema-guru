64d38ec9e652812c6c62c356e9f11783d3af2e7b
Montywilliams <3650@holbertonschool.com>
2023-11-14 13:42:31 -0600

    add props and stiyle for button; add onClick event

--- src/components/general/Button.js
+++ src/components/general/Button.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon from Font Awesome
 
-export default function Button() {
+
+export default function Button({label, className, onClick, icon}) {
   return (
-    <div>
-      Button
-    </div>
+    <button className={`button ${className}`} onClick={onClick}>
+  {icon && (
+    <FontAwesomeIcon icon={icon} className="button-icon" />
+  )}
+  {label}
+</button>
   )
 }
