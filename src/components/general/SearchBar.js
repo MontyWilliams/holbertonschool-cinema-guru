82abcf47fb88db70dde4f74ae55af238be751ef2
Montywilliams <3650@holbertonschool.com>
2023-11-14 13:46:45 -0600

    add className to serch props; create input

--- src/components/general/SearchBar.js
+++ src/components/general/SearchBar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function SearchBar({title, setTitle}) {
+
+export default function SearchBar({title, setTitle, className}) {
   return (
-    <div>
-      SearchBar
+    <div className="search-bar">
+      <FontAwesomeIcon icon={faSearch} className="search-icon" />
+      <input
+        type="text"
+        placeholder={title}
+        value={setTitle}
+        onChange={(e) => setTitle(e.target.value)}
+        className={`search-input ${className}`}
+      />
     </div>
   )
 }
